7b7c99096d7eef467291f2063790bd13dee8de4a
Sebastian <22sevago@gmail.com>
2025-03-12 16:45:11 -0500

    cookies fixed

--- app/controllers/product_controller.py
+++ app/controllers/product_controller.py
@@ -6,20 +6,35 @@ from app.controllers.base_auth_controller import BaseController
 class ProductController(BaseController):
     @staticmethod
     def list_products():
+        
+        user = BaseController.get_authenticated_user()
+        if not user:
+            return jsonify({"success": False, "message": "No autorizado"}), 401       
+         
         """Lista todos los productos en la vista de administración"""
         products = ProductService.get_all_products()
         return render_template("admin/products.html", products=products)
 
     @staticmethod
     def new_product_form():
+        user = BaseController.get_authenticated_user()
+        if not user:
+            return jsonify({"success": False, "message": "No autorizado"}), 401
+        
         """Muestra el formulario de creación de productos"""
         form = ProductForm()
         categories = ProductService.get_all_categories()  # Obtener categorías para el select
+
         form.categoria_id.choices = [(c.id, c.nombre) for c in categories]
+        
         return render_template("admin/product_form.html", form=form)
 
     @staticmethod
     def create_product():
+        user = BaseController.get_authenticated_user()
+        if not user:
+            return jsonify({"success": False, "message": "No autorizado"}), 401
+    
         """Crea un nuevo producto"""
         form = ProductForm()
         categories = ProductService.get_all_categories()
--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -13,8 +13,6 @@ def create_user():
 
     form.tipo_cliente_id.choices = [(tipo.id, tipo.nombre) for tipo in client_types]
 
-
-
     if request.method == "POST" and form.validate_on_submit():
         data = {key: value for key, value in form.data.items() if key not in ["submit", "csrf_token"]}
 
