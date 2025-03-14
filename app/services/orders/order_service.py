47a4b3512b1f13b770764c74ddd36c7fcfd6553f
Sebastian <22sevago@gmail.com>
2025-03-14 16:43:53 -0500

    Pending fix error handling

--- app/services/orders/order_service.py
+++
@@ -1,8 +0,0 @@
-from app.services.orders.order_delete_service import OrderManagementService
-from app.services.orders.order_get_service import OrderQueryService
-from app.services.orders.order_save_service import OrderSaveService
-
-
-class OrderService(OrderManagementService, OrderQueryService, OrderSaveService):
-    def __init__(self):
-        super().__init__()
